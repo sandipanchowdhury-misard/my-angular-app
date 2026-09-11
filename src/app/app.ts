bc5e186cd358f864c09d0cab3cd6d6b3972c9263
Sandipanchowdhury <sandipanchowdhury212@gmail.com>
2026-09-11 20:40:33 +0530

    StringInterpolation, PropertyDataBinding,EventDataBinding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,49 @@ import { Component } from '@angular/core';
 
 export class App {
 
+  constructor(){
+    setTimeout(() =>{
+      this.customInputType ="checkbox";
+    },3000);
+  }
+
   /*String Interpolation*/
   ProjectName = "Angular";
   title       = "Basic App";
   
+  getCustomerName(){
+    return "Robert Jr.";
+  }
+
+  //Poperty Data Binding
+  customerRole = "Admin";
+
+  message = "Login hours 10 am to 11 am";
+
+  //customInputType ="checkbox";
+  customInputType ="radio";
+
+  btnClick(){
+    console.log("Btn Clicked");
+
+    console.log(this.Empname);
+    
+  }
+
+
+  placeholderMessage = "Please Enter your name";
+
+  Empname = "";
   
+  captureData(evtDetails : KeyboardEvent){
+    //console.log("you enter some char.");
+    const inputEl = evtDetails.target as HTMLInputElement
+    
+    //console.log(inputEl.value);
+
+    //console.dir(inputEl);
+    this.Empname = inputEl.value;
+    
+  }
+
 }
