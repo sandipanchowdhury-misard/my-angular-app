3f177046a296d1c2bc5d792e707dc29b35a02c5f
Sandipanchowdhury <sandipanchowdhury212@gmail.com>
2026-09-18 18:20:59 +0530

    *ngSttyle,*ngClass,*ngIf

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('my-angular-app');
+  divBGColor = 'lightgreen';
+
+  divColor = 'blue';
+
+  isSucces= true;
+  divSuccessClass = "success";
+  divFailureClass="failure";
+
+  setSuccess(value:boolean){
+    this.isSucces=value
+  }
+ 
+
+  isStudentPass = "Pass"
 }
