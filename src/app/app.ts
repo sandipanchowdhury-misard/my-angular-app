cfa722dbe07dfa0141cc7a99dc538e807147ed2b
Sandipanchowdhury <sandipanchowdhury212@gmail.com>
2026-09-19 09:03:09 +0530

    *ngFor, *ngSwitch,Case,Default

--- src/app/app.ts
+++ src/app/app.ts
@@ -24,4 +24,63 @@ export class App {
  
 
   isStudentPass = "Pass"
+
+
+  Employees =["Jhon","Robert","Peter","Sandipan"];
+  employeeDetails=[
+  {
+    Name : "Jhon",
+    Salary : "12K"
+  },
+  {
+    Name : "Robert",
+    Salary : "24K"
+  },
+  {
+    Name : "Peter",
+    Salary : "32K"
+  },
+  {
+    Name : "Sandipan",
+    Salary : "55K"
+  }
+];
+
+
+
+  customerID = 10;
+
+  findCustomer(id:string){
+    this.customerID = Number(id)
+  }
+
+
+
+  // weekDaysDetails =[
+  //   {No: 1, Day: "Sunday"},
+  //   {No: 2, Day: "Monday"},
+  //   {No: 3, Day: "Tuesday"},
+  //   {No: 4, Day: "Wednesday"},
+  //   {No: 5, Day: "Thirsday"},
+  //   {No: 6, Day: "Friday"},
+  //   {No: 7 ,Day: "Saturday"}
+  // ];
+
+  // dayNo = 0;
+  // selectedDay : any = null;
+
+  // findDay(id:string){
+  //   if(!id){
+  //     this.selectedDay = null;
+  //     return;
+  //   }
+
+  //   const num = Number(id);
+  //   console.log("Searching for:", this.dayNo);
+  //   this.selectedDay = this.weekDaysDetails.find(d => d.No === this.dayNo);
+  //   console.log("Result:", this.selectedDay);
+  // }
+
+
+
 }
